81951c4573ca81ff495ab7a96083984f9e84d962
agentix-sdk <prince@polkassembly.io>
2025-05-14 13:47:04 +0530

    fix: evm wallet

--- packages/wallets/evm/src/EVMWalletClient.ts
+++ packages/wallets/evm/src/EVMWalletClient.ts
@@ -1,8 +1,19 @@
-import { type EvmChain, type Signature, WalletBase } from "agentix";
+import { type EvmChain, type Signature, EvmWalletBase } from "agentix";
 import type { EVMReadRequest, EVMReadResult, EVMTransaction, EVMTypedData } from "./types";
 
-export abstract class EVMWalletClient extends WalletBase {
-    abstract getChain(): EvmChain;
+export type EVMWalletClientCtorParams = {};
+
+export abstract class EVMWalletClient extends EvmWalletBase {
+    constructor(params: EVMWalletClientCtorParams) {
+        super();
+    }
+
+    getChain() {
+        return {
+            type: "evm",
+        } as EvmChain;
+    }
+
     abstract sendTransaction(transaction: EVMTransaction): Promise<{ hash: string }>;
     abstract read(request: EVMReadRequest): Promise<EVMReadResult>;
     abstract signTypedData(data: EVMTypedData): Promise<Signature>;
